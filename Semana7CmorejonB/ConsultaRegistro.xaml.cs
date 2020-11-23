a41ce1bea94bde5f0f5eabe727db424da57c5fa2
ClaudiaMorejon <claudia.github@gmail.com>
2020-11-22 20:04:03 -0500

    update,delete

--- Semana7CmorejonB/ConsultaRegistro.xaml.cs
+++ Semana7CmorejonB/ConsultaRegistro.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Semana7CmorejonB.Models;
+using SQLite;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,14 +15,41 @@ namespace Semana7CmorejonB
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ConsultaRegistro : ContentPage
     {
+        private SQLiteAsyncConnection _conn;
+        private ObservableCollection<Estudiante> _Tablaestudiante;
         public ConsultaRegistro()
         {
             InitializeComponent();
+            _conn = DependencyService.Get<Database>().GetConnection();
+            NavigationPage.SetHasBackButton(this, false);
+        }
+        protected async override void OnAppearing()
+        {
+            var ResulRegistros = await _conn.Table<Estudiante>().ToListAsync();
+            _Tablaestudiante = new ObservableCollection<Estudiante>(ResulRegistros);
+            ListaUsuario.ItemsSource = _Tablaestudiante;
+            base.OnAppearing();        
         }
 
+        void OnSelection(object sender,SelectedItemChangedEventArgs e)
+        {
+            var obj = (Estudiante)e.SelectedItem;
+            var item = obj.Id.ToString();
+            int ID = Convert.ToInt32(item);
+            try
+            {
+                Navigation.PushAsync(new Elemento(ID));
+            }
+            catch(Exception )
+            {
+                throw;
+            }
+        }
         private void ListaUsuario_ItemSelected(object sender, SelectedItemChangedEventArgs e)
         {
 
         }
+
+        
     }
 }
---
+++ Semana7CmorejonB/Elemento.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Semana7CmorejonB
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Elemento : ContentPage
+    {
+        public Elemento()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_actualizar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_eliminar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
